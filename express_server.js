7b37d678fdedd973eca3d5e2be29789ea3414e6a
Zixia Lu <zixialu@gmail.com>
2018-12-04 13:11:40 -0500

    Implement body-parser and create route to new url form

--- express_server.js
+++ express_server.js
@@ -1,25 +1,36 @@
-const express = require("express");
+const express = require('express');
+const bodyParser =  require('body-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 // Set view engine to ejs
 app.set('view engine', 'ejs');
 
+// Set body parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// Url list
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
+// New url form
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+// View single url
+app.get('/urls/:id', (req, res) => {
   const shortURL = req.params.id;
   const longURL = urlDatabase[shortURL];
   let templateVars = { shortURL, longURL };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 // app.get("/", (req, res) => {
