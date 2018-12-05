b2cfb03c64b3958d4263b4da95a599ed0afb1fb5
Zixia Lu <zixialu@gmail.com>
2018-12-04 19:17:10 -0500

    Include cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser =  require('body-parser');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
